2f3a4f4ee6929a8fb922384c61e40d58ba120427
DrusGalkin <sveta.galkina2021@gmail.com>
2024-11-14 20:26:44 +0300

    init

---
+++ frontend/src/lib/Router/Profile/object.svelte
@@ -0,0 +1,242 @@
+<script>
+    import { onMount } from 'svelte';
+    import { addToFavorites } from '../scripts/favorites'; 
+    import Cookies from 'js-cookie';
+    import axios from "axios";
+    import imghg from '../../../assets/main-photo.png';
+    import like from '../../../assets/like.png';
+
+    export let filteredApartments; 
+    let isLoggedIn = false;
+    let objectTypeName = 'Тип объекта';
+    let seaDistance = 0; 
+
+    async function addToFavoritesHandler(apartment) {
+        if (isLoggedIn) {
+            try {
+                const response = await axios.get(`/api/objects/${apartment.id}/add_to_favorites/`);
+                console.log('Добавлено в избранное:', response.data);
+            } catch (error) {
+                console.error('Ошибка при добавлении в избранное:', error.response ? error.response.data : error.message);
+            } 
+        } else {
+            addToFavorites(apartment.id);
+            console.log('Сохранено в куки:', apartment.id);
+        }
+    }
+
+    async function fetchObjectTypeDetails(typeId) {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/types-of-objects/${typeId}/`);
+            objectTypeName = response.data.name;
+            console.log('Данные о типе объекта:', objectTypeName);
+        } catch (error) {
+            console.error('Ошибка при получении данных о типе объекта:', error.response ? error.response.data : error.message);
+        }
+    }
+
+    onMount(() => {
+        isLoggedIn = !!Cookies.get('token'); 
+
+        if (filteredApartments.length > 0) {
+            const apartment = filteredApartments[0];
+            const typeId = apartment.type;
+            seaDistance = apartment.seaDistance; 
+            fetchObjectTypeDetails(typeId); 
+        }
+        
+    });
+</script>
+
+{#if filteredApartments.length > 0}
+<main>
+    <div class="photo">
+        <img id="middlePhoto" src="{filteredApartments[0].images.length > 0 ? filteredApartments[0].images[0] : imghg}" alt="">
+    </div>
+
+    <div class="info">
+        <div class="blockInfo">
+            <div class="nameAdress">
+                <h1>
+                    {objectTypeName} «{filteredApartments[0].title}»
+                </h1>
+                <p>
+                    {filteredApartments[0].city}, {filteredApartments[0].street}
+                </p>
+
+                <p>Море - {seaDistance > 999 ? (seaDistance / 1000).toFixed(1) + " км" : seaDistance + " м"}</p>
+            </div>
+            <img id="like" on:click={() => addToFavoritesHandler(filteredApartments[0])} src="{like}" alt="">
+        </div>
+        <div class="block">
+            <div class="tags">
+                <div class="fullTags">
+                    {#if filteredApartments[0].tags.length > 0}
+                            {#each filteredApartments[0].tags.slice(0, 6) as tag}
+                                {tag}
+                            {/each}
+                    {:else}
+                        <p>Нет тегов</p>
+                    {/if}
+                </div>
+    
+                <div class="raiting">
+                   <p>
+                        <span>5.0</span> 33 отзыва
+                   </p>  
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="price">
+        <h1>От { Math.floor(filteredApartments[0].minPrice) } руб</h1>
+        <p>цена за сутки</p>
+
+        <section>
+            <button id="blueButton">Подробнее</button>
+        </section>
+    </div>
+</main>
+{:else}
+    <p>Нет доступных квартир</p>
+{/if}
+<style>
+    .block{
+        height: 70%;
+        display: flex;
+        align-items: end;
+    }
+
+    .price h1{
+        font-size: 17px;
+        font-weight: 400;
+        line-height: 20.15px;
+        align-self: flex-start;
+    }
+
+    .price p{
+        font-size: 12px;
+        font-weight: 300;
+        line-height: 14.22px;
+        align-self: flex-start;
+    }
+
+    #blueButton{
+        width: 102px;
+        height: 27px;
+        border-radius: 5px;
+        background-color: var(--color);
+        color: white;
+        font-family: Rubik;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 14.22px;
+    }
+
+    section{
+        display: flex;
+        justify-content: center;
+        align-items: end;
+        height: 80%;
+    }
+
+    .raiting span{
+        background: #00C12A;
+        font-family: Rubik;
+        font-size: 12px;
+        font-weight: 300;
+        line-height: 14.22px;
+        color: white;
+        padding: 0 5px 0 5px;
+        border-radius: 10px;
+    }
+
+    .raiting p{
+        font-family: Rubik;
+        font-size: 12px;
+        font-weight: 300;
+        line-height: 14.22px;
+    }
+
+    .price{
+        display: flex;
+        padding: 0 10px 0 10px;
+        align-items: center;
+        margin: 20px 0 20px 0;
+        height: auto;
+        flex-direction: column;
+        border-left: 2px rgba(128, 128, 128, 0.315) solid;
+    }
+
+    .fullTags{
+        display: flex;
+        gap: 10px;
+        font-size: 12px;
+        font-weight: 300;
+        line-height: 14.22px;
+        margin-bottom: 10px;
+    }
+
+    .info{
+        max-width: 400px;
+        flex-grow: 1;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        flex-direction: column;
+        height: auto;
+        margin-left: 10px;
+    }
+
+    .blockInfo{
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        flex-grow: 1;
+    }
+
+    .nameAdress h1{
+        font-size: 18px;
+        font-weight: 400;
+        line-height: 21.33px;
+        padding-bottom: 10px;
+    }
+
+    .nameAdress p{
+        font-size: 12px;
+        font-weight: 300;
+        line-height: 14.22px;
+        text-decoration-style: solid;
+        text-underline-position: from-font;
+        text-decoration-skip-ink: auto;
+    }
+
+    main{
+        max-width: 768px;
+        height: auto;
+        border: 1px rgba(128, 128, 128, 0.315) solid;
+        border-radius: 10px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        gap: 10px;
+    }
+
+    .photo {
+        width: 262px; 
+        height: 243px;
+        overflow: hidden; 
+    }
+
+    #like{
+        width: 30px;
+        height: 27px;
+    }
+
+    #middlePhoto{
+        width: 100%; 
+        height: 100%; 
+        border-radius: 10px;
+        object-fit: cover; 
+    }
+</style>
---
+++ frontend/src/lib/Router/search.svelte
@@ -0,0 +1,211 @@
+<script>
+    import { onMount } from 'svelte';
+    import { useLocation } from 'svelte-routing';
+    import SearchInput from './Routing_lib/searchInput.svelte';
+    import Header from '../Header.svelte';
+    import { navigate } from 'svelte-routing';
+    import axios from 'axios';
+    import Object from './Profile/object.svelte';
+    import Search from './Main/search.svelte';
+
+    let city = '';
+    let adults = '';
+    let first_day = '';
+    let last_day = '';
+    let kids = '';
+    let apartments = [];
+    let filteredApartments = [];
+    let toNavigate = true;
+
+    const location = useLocation();
+
+    async function goHome() {
+        navigate(`/`);
+    }
+
+    async function fetchCityName(cityId) {
+        try {
+            const response = await axios.get(`http://localhost:8000/api/city/${cityId}/`);
+            return response.data.name; 
+        } catch (error) {
+            console.error('Ошибка при получении названия города:', error);
+            return null; 
+        }
+    }
+
+    async function fetchObjects() {
+        try {
+            const response = await axios.get('http://localhost:8000/api/objects/');
+            apartments = await Promise.all(response.data.map(async (object) => {
+                const cityName = await fetchCityName(object.address.city);
+                return {
+                    id: object.id,
+                    city: cityName || object.address.city,
+                    street: object.address.street,
+                    house: object.address.house,
+                    seaDistance: object.address.sea_distance,
+                    title: object.name,
+                    description: object.description || "Нет описания",
+                    minPrice: object.min_price,
+                    type: object.type,
+                    images: object.images.length > 0 ? object.images.map(image => image.media) : [],
+                    tags: object.tags.map(tag => tag.title),
+                };
+            }));
+            filteredApartments = apartments; // Изначально все квартиры отображаются
+        } catch (error) {
+            console.error('Ошибка при получении данных:', error);
+        }
+    }
+
+    function updateFilteredApartments() {
+        filteredApartments = apartments.filter(apartment => apartment.city === city);
+    }
+
+    onMount(async () => {
+        const params = new URLSearchParams(window.location.search);
+        city = params.get('city') || '';
+        adults = params.get('adults') || '';
+        first_day = params.get('first_day') || '';
+        last_day = params.get('last_day') || '';
+        kids = params.get('kids') || '';
+
+        await fetchObjects();
+        updateFilteredApartments();
+    });
+
+    let activeTab = "popular"
+
+ 
+    const switchTab = (tab) => {
+        activeTab = tab
+        console.log(activeTab)
+    };
+</script>
+
+<main>
+    <Header toNavigate={toNavigate} />
+    <div class="block">
+        <nav>
+            <a href="" on:click={goHome} id="middle">Главная</a> <p> > </p> <p>{city}</p>
+        </nav>
+        <section>
+            <Search
+                city={city}
+                adults={adults}
+                checkInDate={first_day}
+                checkOutDate={last_day}
+                on:search={() => updateFilteredApartments()} 
+            />
+        </section>
+    </div>
+
+    <div class="block2">
+        <div class="blockFilters">
+            asads
+        </div>
+
+        <div class="blockPage">
+            <div class="info">
+                <p>Найдено {filteredApartments.length} варианта жилья</p>
+
+                <div class="sort">
+                    <a on:click={() => {switchTab("popular")}}>По популярности</a>
+                    <a on:click={() => {switchTab("raiting")}}>По рейтингу</a>
+                    <a on:click={() => {switchTab("nerdeSea")}}>Ближе к морю</a>
+                </div>
+            </div>
+
+            <Object {filteredApartments} />
+
+        </div>
+    </div>
+</main>
+
+<style>
+    .blockPage{
+        display: flex;
+        max-width: 817px;
+        gap: 30px;
+        flex-grow: 1;
+        flex-direction: column;
+        background: #F5F5F5;
+        padding: 25px;
+        border: 2px rgba(128, 128, 128, 0.315) solid;
+        border-radius: 15px;
+    }
+
+    .info{
+        display: flex;
+        justify-content: space-between;
+        color: #959595;
+        font-size: 14px;
+        font-weight: 300;
+        line-height: 16.59px;
+    }
+
+    .sort{
+        display: flex;
+        justify-content: space-between;
+        gap: 10px;  
+    }
+
+    .block2{
+        padding-top: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        flex-grow: 1;
+        margin: 0 auto;
+        width:100%;
+    }
+
+    .block{
+        margin-top: 50px;
+    }
+    
+    nav{
+        display: flex;
+        gap: 10px;
+        width: 90%;
+
+        padding: 20px 0 20px 5px;
+    }
+
+    nav a{
+        text-decoration: none;
+        color: black;
+        transition: all 0.3s ease 0s;
+        cursor: cell;
+    }
+
+    nav #middle:hover{
+        transform: scale(1.2);
+        color: gray;
+        cursor: pointer;
+    }
+
+    section h1{
+        color: white;
+        font-size: clamp(30px,2vw,50px);
+        margin-bottom: 30px;
+    }
+
+    section{
+        width: 100%;
+        background: var(--color);
+        padding: 20px 0px 20px 0px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        border-radius: 20px;
+    }
+
+    @media(max-width:1498px){
+        section{
+            border-radius: 0px;
+        }
+    }
+</style>
